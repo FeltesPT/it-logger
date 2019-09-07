bccdae81b59af234ff4c4fc29ff96b1ebfcb60c5
Tiago Dias <tiago.dias@equiis.com>
2019-09-07 21:46:40 +0100

    Add and update and delete logs

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { updateLog } from '../../actions/logActions';
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
 	const [message, setMessage] = useState('');
 	const [attention, setAttention] = useState(false);
 	const [tech, setTech] = useState('');
+
+	useEffect(() => {
+		if (current) {
+			setMessage(current.message);
+			setTech(current.tech);
+			setAttention(current.attention);
+		}
+	}, [current]);
 
 	const onSubmit = () => {
 		if (message === '' || tech === '') {
 			M.toast({ html: 'Please enter a message and tech' });
 		} else {
-			console.log(message, tech, attention);
+			const updatedLog = {
+				id: current.id,
+				message,
+				attention,
+				tech,
+				date: new Date()
+			};
+
+			updateLog(updatedLog);
+
+			M.toast({ html: `Log Updated by ${tech}` });
 
 			// Clear fields
 			setMessage('');
@@ -26,9 +47,6 @@ const EditLogModal = () => {
 				<div className='row'>
 					<div className='input-field'>
 						<input type='text' name='message' value={message} onChange={e => setMessage(e.target.value)} />
-						<label htmlFor='message' className='active'>
-							Log Message
-						</label>
 					</div>
 				</div>
 				<div className='row'>
@@ -79,4 +97,16 @@ const modalStyle = {
 	height: '75%'
 };
 
-export default EditLogModal;
+EditLogModal.propTypes = {
+	current: PropTypes.object,
+	updateLog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+	current: state.log.current
+});
+
+export default connect(
+	mapStateToProps,
+	{ updateLog }
+)(EditLogModal);
